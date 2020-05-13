f804ca8b4df8faed2f35a8883f3732b4c7a47355
wangjianxiong <wangjianxiong@eoffice.com>
2020-05-13 22:03:47 +0800

    测试http请求

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,6 +11,7 @@ import { AppRoutesModule } from './app.routing';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutesModule,
   ],
   providers: [],
